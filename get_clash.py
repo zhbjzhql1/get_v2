46e35904cbb85bf655462225cb5ff7fab5d24e47
Mirei <42766108+mahdibland@users.noreply.github.com>
2022-09-29 09:54:22 +0330

    Update get_clash.py

--- get_clash.py
+++ get_clash.py
@@ -10,19 +10,18 @@ from utils.cfmem import get_content as cfmem_content
 from utils.pawdroid import get_content as pawdroid_content
 from utils.mattkaydiary import get_content as mattkaydiary_content
 
-pathToYaml = json.loads(requests.get('https://api.github.com/repos/changfengoss/pub/git/trees/main?recursive=1').text)["tree"][-1]["path"]
-pathToYamlsplit = pathToYaml.split('/')
-changfengoss = os.path.join("changfengoss/%s/%s" % (pathToYamlsplit[0],pathToYamlsplit[1]))
-print(changfengoss)
-dirname = time.strftime("%Y_%m_%d", time.localtime(time.time()))
-yamlUtils = YamlUtils(changfengoss)
-yamlUtils.clone_repo("https://ghproxy.com/https://github.com/changfengoss/pub.git")
-# yamlUtils.make_template_dict("yaml", dirname)
-# with open("pub/changfengoss.yaml", "w", encoding="utf8") as outfile:
-#     outfile.write(template, pathToYaml)
-# yamlUtils.save_file()
-copyfile(os.path.join(changfengoss, pathToYamlsplit[2]), "pub/changfengoss.yaml")
-shutil.rmtree(changfengoss)
+pathToYamllist = json.loads(requests.get(
+    'https://api.github.com/repos/changfengoss/pub/git/trees/main?recursive=1').text)["tree"]
+pathToYamllist = pathToYamllist[-1]["path"]
+date = pathToYaml.split('/')[1]
+yamls = list(filter(lambda x: x["path"].__contains__(date) and x["path"].endswith(".yaml"), iterable))
+
+for (index, item) in enumerate(yamls):
+    data = requests.get("https://raw.githubusercontent.com/changfengoss/pub/main/%s" % item["path"]).text
+    
+    with open("pub/changfengoss%d.yaml" % index, 'w') as output:
+        output.write(data)
+        
 
 bhqz = os.path.join("bhqz")
 yamlUtils = YamlUtils(bhqz)
