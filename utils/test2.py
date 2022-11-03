51caf455b30239cbed57e6fd2c1620d081cae4bf
Mirei <42766108+mahdibland@users.noreply.github.com>
2022-11-03 09:48:46 +0330

    Update test2.py

--- utils/test2.py
+++ utils/test2.py
@@ -1,7 +1,17 @@
 import requests
 
-local = '0.0.0.0:3333'
+local1 = 'http://127.0.0.1:3333'
+local2 = 'http://0.0.0.0:3333'
 
-response = requests.get(f'{local}/get-base64?content=https://raw.githubusercontent.com/MOnday9907/v2ray/main/v2ray.txt').text
+try:
+  response1 = requests.get(f'{local1}/get-base64?content=https://raw.githubusercontent.com/MOnday9907/v2ray/main/v2ray.txt').text
+except:
+  pass
+try:
+  response2 = requests.get(f'{local2}/get-base64?content=https://raw.githubusercontent.com/MOnday9907/v2ray/main/v2ray.txt').text
+except: 
+  pass
 
-print(response)
+print(response1)
+print('-----------------------------')
+print(response2)
